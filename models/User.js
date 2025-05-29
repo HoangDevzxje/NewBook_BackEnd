9ee5136a5e0618d04c345ef8497f804d5e65c276
Minh Dong <nvdong0902@gmail.com>
2025-05-29 14:59:20 +0700

    Create model

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true, required: true },
+  password: String,
+  phone: String,
+  address: [
+    {
+      address: { type: String, required: true },
+      provineName: { type: String, required: true },
+      districtName: { type: String, required: true },
+      wardName: { type: String, required: true },
+    }
+  ],
+  point: { type: Number, default: 0 },
+  wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Book' }],
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  isActivated: {
+    type: Boolean,
+    default: true
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
