11a8234467ef6ac658dd7cf153f66332bac9fdf0
Minh Dong <nvdong0902@gmail.com>
2025-05-31 17:15:48 +0700

    commit user wish list

---
+++ controllers/UserController.js
@@ -0,0 +1,74 @@
+const bcrypt = require("bcryptjs");
+const Book = require("../models/Book");
+const Complaint = require("../models/Complaint");
+const User = require("../models/User");
+
+// wishlist
+const addBookToWishlist = async (req, res) => {
+    try {
+        const { bookId } = req.params;
+        const user = req.user;
+
+        // Kiểm tra sách có tồn tại không
+        const book = await Book.findById(bookId);
+        if (!book) {
+            return res.status(404).json({ message: "Sách không tồn tại!" });
+        }
+
+        // Kiểm tra nếu sách đã có trong wishlist
+        if (user.wishlist.includes(bookId)) {
+            return res.status(400).json({ message: "Sách đã có trong wishlist!" });
+        }
+
+        // Thêm sách vào wishlist
+        user.wishlist.push(bookId);
+        await user.save();
+
+        res
+            .status(200)
+            .json({ message: "Đã thêm sách vào wishlist!", wishlist: user.wishlist });
+    } catch (error) {
+        res.status(500).json({ message: "Lỗi server!", error: error.message });
+    }
+};
+
+const deleteBookFromWishlist = async (req, res) => {
+    try {
+        const { bookId } = req.params;
+        const user = req.user;
+
+        // Kiểm tra nếu sách không có trong wishlist
+        if (!user.wishlist.includes(bookId)) {
+            return res.status(400).json({ message: "Sách không có trong wishlist!" });
+        }
+
+        // Loại bỏ sách khỏi wishlist
+        user.wishlist = user.wishlist.filter((id) => id.toString() !== bookId);
+        await user.save();
+
+        res
+            .status(200)
+            .json({ message: "Đã xóa sách khỏi wishlist!", wishlist: user.wishlist });
+    } catch (error) {
+        res.status(500).json({ message: "Lỗi server!", error: error.message });
+    }
+};
+
+const getMyWishlist = async (req, res) => {
+    try {
+        const user = await User.findById(req.user._id).populate("wishlist");
+        if (user.wishlist.length === 0) {
+            return res.status(404).json({ message: "Không có yêu thích nào!" });
+        }
+        res.status(200).json({ wishlist: user.wishlist });
+    } catch (error) {
+        res.status(500).json({ message: "Lỗi server!", error: error.message });
+    }
+};
+
+
+module.exports = {
+    addBookToWishlist,
+    deleteBookFromWishlist,
+    getMyWishlist,
+};
